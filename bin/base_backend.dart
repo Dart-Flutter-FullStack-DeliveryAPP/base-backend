292955933baed4e4369c22403597ed00afb7fb54
Lucas Menezes <lucas.menezesc120@gmail.com>
2022-08-04 10:24:12 -0300

    Add Dependencies

--- bin/base_backend.dart
+++ bin/base_backend.dart
@@ -1,5 +1,12 @@
-import 'package:base_backend/base_backend.dart' as base_backend;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:mysql1/mysql1.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${base_backend.calculate()}!');
+  serve(
+    (Request req) => Response(200,
+        body: 'Olá Mundo', headers: {'content-type': 'application/json'}),
+    'localhost',
+    4040,
+  );
 }
